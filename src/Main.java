ecc7b5bc3497e3166a9a46ca708c7519c4086944
luan-vrrbr <luan.vrrbr@eduge.ch>
2022-06-10 09:47:48 +0200

    Modification en nom Mathias

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("Je m'appelle Luan");
+        System.out.println("Je m'appelle Mathias");
 
     }
 }
